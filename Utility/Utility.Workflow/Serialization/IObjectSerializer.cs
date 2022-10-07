850096572f890ff9323d8219fe816ea73c45e086
J. Mayr <jsmayr@online.de>
2022-10-07 22:59:03 +0200

    Initial commit.

---
+++ Utility/Utility.Workflow/Serialization/IObjectSerializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Utility.Workflow.Serialization
+{
+    public interface IObjectSerializer
+    {
+        void Serialize<T>(Stream stream, T instance, IServiceProvider context);
+        T Deserialize<T>(Stream stream, IServiceProvider context);
+    }
+}
