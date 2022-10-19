fe36fc590fc57c0588129bfb15467220cf22b2c2
J. Mayr <jsmayr@online.de>
2022-10-19 16:11:14 +0200

    Added Pinned-object

---
+++ Utility/Utility.Workflow/Pinned.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Utility.Workflow
+{
+    public sealed class Pinned<T>
+        where T : unmanaged
+    {
+        private readonly T _instance;
+        private GCHandle _handle;
+
+        private int _allocationCount;
+        private readonly object _syncObject;
+
+        public Pinned(T instance, bool synchronized = true)
+        {
+            _instance = instance;
+            _syncObject = synchronized ? new object() : null;
+        }
+
+        public T Instance => _instance;
+
+        public bool IsPinned => _allocationCount > 0;
+
+        public PinnedReference<T> Allocate()
+        {
+            if (_allocationCount == 0)
+            {
+                if (_syncObject == null)
+                {
+                    _allocationCount++;
+                    _handle = GCHandle.Alloc(_instance, GCHandleType.Pinned);
+                }
+                else
+                {
+                    lock (_syncObject)
+                    {
+                        if (_allocationCount++ == 0)
+                            _handle = GCHandle.Alloc(_instance, GCHandleType.Pinned);
+                    }
+                }
+            }
+
+            return new PinnedReference<T>(this);
+        }
+
+        internal unsafe T* InternalPointer
+        {
+            get
+            {
+                if (_allocationCount == 0)
+                    ThrowNotAllocated();
+
+                return (T*)_handle.AddrOfPinnedObject();
+            }
+        }
+
+        internal void InternalFreeOnce()
+        {
+            if (_syncObject == null)
+            {
+                if (--_allocationCount == 0)
+                {
+                    _handle.Free();
+                    _handle = default;
+                }
+            }
+            else
+            {
+                lock (_syncObject)
+                {
+                    if (--_allocationCount == 0)
+                    {
+                        _handle.Free();
+                        _handle = default;
+                    }
+                }
+            }
+        }
+
+        ~Pinned()
+        {
+            if (_allocationCount > 0)
+                _handle.Free();
+        }
+
+        private static unsafe void ThrowNotAllocated()
+        {
+            throw new InvalidOperationException("The current instance is not allocated.");
+        }
+    }
+}
---
+++ Utility/Utility.Workflow/PinnedReference.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+namespace Utility.Workflow
+{
+    public sealed class PinnedReference<T> : IDisposable
+        where T : unmanaged
+    {
+        private readonly Pinned<T> _handle;
+        private int _disposed;
+
+        internal PinnedReference(Pinned<T> handle)
+        {
+            _handle = handle;
+        }
+
+        public T Instance => _handle.Instance;
+
+        public unsafe T* Pointer
+        {
+            get
+            {
+                ThrowDisposed();
+
+                return _handle.InternalPointer;
+            }
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (Interlocked.Exchange(ref _disposed, 1) == 0)
+                _handle.InternalFreeOnce();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void ThrowDisposed()
+        {
+            if (_disposed == 1)
+                throw new ObjectDisposedException(this.GetType().Name);
+        }
+
+        ~PinnedReference()
+        {
+            Dispose(false);
+        }
+
+        public unsafe static explicit operator IntPtr(PinnedReference<T> pinned) => (IntPtr)pinned.Pointer;
+    }
+}
